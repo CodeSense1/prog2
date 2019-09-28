b97f7b9ca65e518225266bc8467a30d980ae4d9d
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2019-09-28 20:41:39 +0300

    Game 2048

--- student/04/numbers/main.cpp
+++ student/04/numbers/main.cpp
@@ -44,18 +44,25 @@ const int DEFAULT_GOAL = 2048;
 using Board = std::vector<std::vector<NumberTile>>;
 
 // enum for different directions where tiles can move
-enum DIR {W, A, S, D, NONE};
-
+enum DIR
+{
+    W,
+    A,
+    S,
+    D,
+    NONE
+};
 
 // Adds a single new value to board using rEng and distr for random positioning.
-void newValue(Board* board,
+void newValue(Board *board,
               std::default_random_engine &rEng,
-              std::uniform_int_distribution<int> &distr){
-
+              std::uniform_int_distribution<int> &distr)
+{
 
     // Tries to assign NEW_VAl to randomly selected tile. Continues trying until
     // newVal() returns true.
-    while(!board->at(distr(rEng)).at(distr(rEng)).setValue(NEW_VALUE, true));
+    while (!board->at(distr(rEng)).at(distr(rEng)).setValue(NEW_VALUE, true))
+        ;
 }
 
 // Initializes the board to size SIZE x SIZE and adds SIZE tiles with NEW_VALUE
@@ -63,19 +70,20 @@ void newValue(Board* board,
 // a seed value.
 void initBoard(Board &board,
                std::default_random_engine &rEng,
-               std::uniform_int_distribution<int> &distr) {
-
+               std::uniform_int_distribution<int> &distr)
+{
 
     // Initialize the board with SIZE x SIZE empty numbertiles.
-    for ( auto y = 0; y < SIZE; y++ ){
+    for (auto y = 0; y < SIZE; y++)
+    {
         board.push_back({});
-        for ( auto x = 0; x < SIZE; x++ ){
+        for (auto x = 0; x < SIZE; x++)
+        {
             // If you don't want to use pairs, replace "std::make_pair(y, x)"
             // with "y, x".
 
             board.at(y).push_back(NumberTile(0, x, y, &board));
         }
-
     }
 
     // Ask user for the seed value and initialize rEng.
@@ -83,31 +91,37 @@ void initBoard(Board &board,
     std::string seed = "";
     getline(std::cin, seed);
 
-
-    if(seed == "") {
+    if (seed == "")
+    {
         // If the user did not give a seed value, use computer time as the seed
         // value.
         rEng.seed(time(NULL));
-    } else {
+    }
+    else
+    {
         // If the user gave a seed value, use it.
         rEng.seed(stoi(seed));
     }
 
     // Add some tiles to the board.
-    for ( int i = 0 ; i < SIZE ; i++ ){
+    for (int i = 0; i < SIZE; i++)
+    {
         newValue(&board, rEng, distr);
     }
 }
 
 // Prints the board.
-void print(Board &board){
+void print(Board &board)
+{
     // The y isn't actually the y coordinate or some int, but an iterator that
     // is like a vector of NumberTiles.
-    for ( auto y : board ){
+    for (auto y : board)
+    {
         // Prints a row of dashes.
         std::cout << std::string(PRINT_WIDTH * SIZE + 1, '-') << std::endl;
         // And then print all cells in the desired width.
-        for ( auto x : y ){
+        for (auto x : y)
+        {
             x.print(PRINT_WIDTH);
         }
         // And a line after each row.
@@ -117,8 +131,8 @@ void print(Board &board){
     std::cout << std::string(PRINT_WIDTH * SIZE + 1, '-') << std::endl;
 }
 
-
-void slide( std::vector<NumberTile>& arr ) {
+void slide(std::vector<NumberTile> &arr)
+{
     /*  Slides one row. It does not combine, so you might have
      *  situation where 2 0 2 0 results 2 2 0 0
      *  Values greater than zero will be moved towards the beginning of the
@@ -132,34 +146,38 @@ void slide( std::vector<NumberTile>& arr ) {
 
     std::vector<int> values = {};
 
-
     for (auto i : arr)
         values.push_back(i.getValue());
 
     auto copy = values;
     int index = 0;
-    for (int k = 0; k < SIZE-1; k++) {
+    for (int k = 0; k < SIZE - 1; k++)
+    {
 
-        if (values.at(index) == 0) {
+        if (values.at(index) == 0)
+        {
             // shift values (if > 0) towards the beginning of the vector
             copy = values;
-            for (int i = index; i < SIZE; i++) {
+            for (int i = index; i < SIZE; i++)
+            {
                 if (i + 1 == SIZE)
                     values.at(i) = copy.at(index);
                 else
-                    values.at(i) = copy.at(i+1);
+                    values.at(i) = copy.at(i + 1);
             }
-
-        } else
+        }
+        else
             index++;
     }
 
-    for ( int v = 0; v < SIZE; v++) {
+    for (int v = 0; v < SIZE; v++)
+    {
         arr.at(v).setValue(values.at(v));
     }
 }
 
-void combine( std::vector<NumberTile>& arr ) {
+void combine(std::vector<NumberTile> &arr)
+{
     /*  Combines two subsequent tiles. Combined tile will be towards
      *  the beginning of the vector. Other tile will be reset to 0.
      *  Modifies given vector accordingly.
@@ -170,17 +188,19 @@ void combine( std::vector<NumberTile>& arr ) {
      *  return: void
      */
 
-    for ( int i = 0; i < arr.size()-1; i++ ) {
-        if (arr.at(i).getValue() == arr.at(i+1).getValue()
-                && arr.at(i).getValue() != 0) {
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        if (arr.at(i).getValue() == arr.at(i + 1).getValue() && arr.at(i).getValue() != 0)
+        {
 
-            arr.at(i).setValue(arr.at(i).getValue() + arr.at(i+1).getValue());
-            arr.at(i+1).setValue(0);
+            arr.at(i).setValue(arr.at(i).getValue() + arr.at(i + 1).getValue());
+            arr.at(i + 1).setValue(0);
         }
     }
 }
 
-bool hasWon(Board board, const int optionalScore) {
+bool hasWon(Board board, const int optionalScore)
+{
 
     /*
      * Returns true if player has reached defaul goal (2048) or
@@ -194,16 +214,19 @@ bool hasWon(Board board, const int optionalScore) {
      */
 
     for (int i = 0; i < SIZE; i++)
-        for (int j = 0; j < SIZE; j++) {
+        for (int j = 0; j < SIZE; j++)
+        {
             if (board.at(j).at(i).getValue() >= optionalScore ||
-                    board.at(j).at(i).getValue() >= DEFAULT_GOAL) {
+                board.at(j).at(i).getValue() >= DEFAULT_GOAL)
+            {
                 return true;
             }
         }
     return false;
 }
 
-bool gameOver(Board board) {
+bool gameOver(Board board)
+{
     /*
      * Checks every tile and it's neighbours. If two neighbours have same value
      * game is not over yet
@@ -211,7 +234,8 @@ bool gameOver(Board board) {
      */
 
     for (int x = 0; x < SIZE; x++)
-        for (int y = 0; y < SIZE; y++) {
+        for (int y = 0; y < SIZE; y++)
+        {
             NumberTile cur = board.at(y).at(x);
 
             if (cur.getValue() == 0)
@@ -219,25 +243,29 @@ bool gameOver(Board board) {
 
             // Check tiles around current tile
             for (int i = -1; i < 2; i++)
-                for (int j = -1; j < 2; j++) {
-                    if(x+i > 0 && x+i < SIZE-1 && y+j > 0 && y+j < SIZE-1) {
+                for (int j = -1; j < 2; j++)
+                {
+                    if (x + i > 0 && x + i < SIZE - 1 && y + j > 0 && y + j < SIZE - 1)
+                    {
                         // There are two tiles with same number, player can make move
-                        if( board.at(y+j).at(x).getValue() == cur.getValue() ||
-                            board.at(y).at(x+i).getValue() == cur.getValue())
+                        if (board.at(y + j).at(x).getValue() == cur.getValue() ||
+                            board.at(y).at(x + i).getValue() == cur.getValue())
                             return false;
                     }
                 }
         }
 
     return true;
-
 }
 
-bool hasEmptySpaces(Board board) {
+bool hasEmptySpaces(Board board)
+{
 
     // check if board has space for new value
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
             if (board.at(j).at(i).getValue() == 0)
                 return true;
         }
@@ -245,22 +273,24 @@ bool hasEmptySpaces(Board board) {
     return false;
 }
 
-
-void update(Board& board, DIR d) {
+void update(Board &board, DIR d)
+{
     // Makes a move on the board and updates all events on the board
 
     // param: board - reference to current game board
     //        d     - direction, where board should be moved
 
-
     // return: void
 
-    if (d == W) {
-        for (int x = 0; x < SIZE; x++) {
+    if (d == W)
+    {
+        for (int x = 0; x < SIZE; x++)
+        {
 
             // Construct column
             std::vector<NumberTile> col = {};
-            for (int y = 0; y < SIZE; y++) {
+            for (int y = 0; y < SIZE; y++)
+            {
                 col.push_back(board.at(y).at(x));
             }
 
@@ -273,12 +303,15 @@ void update(Board& board, DIR d) {
         }
     }
 
-    if (d == A) {
-        for (int y = 0; y < SIZE; y++) {
+    if (d == A)
+    {
+        for (int y = 0; y < SIZE; y++)
+        {
 
             // Construct row
             std::vector<NumberTile> row = {};
-            for (int x = 0; x < SIZE; x++) {
+            for (int x = 0; x < SIZE; x++)
+            {
                 row.push_back(board.at(y).at(x));
             }
 
@@ -288,16 +321,18 @@ void update(Board& board, DIR d) {
             slide(row);
             for (int i = 0; i < SIZE; i++)
                 board.at(y).at(i) = row.at(i);
-
         }
     }
 
-    if (d == S) {
-        for (int x = 0; x < SIZE; x++) {
+    if (d == S)
+    {
+        for (int x = 0; x < SIZE; x++)
+        {
 
             // Construct column
             std::vector<NumberTile> col = {};
-            for( int y = SIZE-1; y > -1; y--) {
+            for (int y = SIZE - 1; y > -1; y--)
+            {
                 col.push_back(board.at(y).at(x));
             }
             // Slide values down
@@ -305,16 +340,18 @@ void update(Board& board, DIR d) {
             combine(col);
             slide(col);
             for (int i = 0; i < SIZE; i++)
-                board.at(i).at(x) = col.at(SIZE-1-i);
+                board.at(i).at(x) = col.at(SIZE - 1 - i);
         }
     }
 
-    if (d == D) {
-        for (int y = 0; y < SIZE; y++) {
+    if (d == D)
+    {
+        for (int y = 0; y < SIZE; y++)
+        {
 
             // Construct row
             std::vector<NumberTile> row = {};
-            for (int x = SIZE-1; x > -1; x--)
+            for (int x = SIZE - 1; x > -1; x--)
                 row.push_back(board.at(y).at(x));
 
             // Slide values to the right
@@ -322,15 +359,14 @@ void update(Board& board, DIR d) {
             combine(row);
             slide(row);
             for (int i = 0; i < SIZE; i++)
-                board.at(y).at(i) = row.at(SIZE-1-i);
-
+                board.at(y).at(i) = row.at(SIZE - 1 - i);
         }
     }
-
 }
 
-int main() {
-    // SETUP //
+int main()
+{
+    ///////////// SETUP /////////////
 
     // Declare the board and randomengine.
     Board board;
@@ -338,48 +374,102 @@ int main() {
     // And initialize the disrt to give numbers from the correct
     std::uniform_int_distribution<int> distr(0, SIZE - 1);
 
-    initBoard(board, randomEng, distr); // initialize board
+    initBoard(board, randomEng, distr);
 
     // Define optional goal before printing the board
     std::string input;
-    std::cout << "Give a goal value or and empty line: ";
+    std::cout << "Give a goal value or an empty line: ";
     getline(std::cin, input);
+    int OPTIONAL_GOAL;
 
-    const int OPTIONAL_GOAL = (input != "") ? std::stoi(input) : DEFAULT_GOAL;
+    try {
+        OPTIONAL_GOAL = (input != "") ? std::stoi(input) : DEFAULT_GOAL;
+    } catch (std::invalid_argument) {
+        OPTIONAL_GOAL = DEFAULT_GOAL;
+    }
+
 
     print(board);
 
     bool running = true;
 
-    // UPDATE
-    while (running) {
+    ///////////// LOOP /////////////
+    while (running)
+    {
         std::cout << "Dir> ";
-        char dir;
+        std::string dir;
         std::cin >> dir;
 
         DIR direction;
 
         // Respond accordingly
-        switch (dir) {
+        switch (static_cast<char>(dir[0]))
+        {
 
-            case 'w': { direction = W; break;      }
-            case 'a': { direction = A; break;      }
-            case 's': { direction = S; break;      }
-            case 'd': { direction = D; break;      }
-            case 'q': { running = false; continue; }
+        case 'w':
+        {
+            direction = W;
+            break;
+        }
+        case 'a':
+        {
+            direction = A;
+            break;
+        }
+        case 's':
+        {
+            direction = S;
+            break;
+        }
+        case 'd':
+        {
+            direction = D;
+            break;
+        }
+        case 'q':
+        {
+            running = false;
+            continue;
+        }
 
-            default: {
-                std::cout <<
-                             "Error: unknown command."
-                          << std::endl;
-                direction = NONE;
-                break;
-            }
+        default:
+        {
+            std::cout << "Error: unknown command."
+                      << std::endl;
+            direction = NONE;
+            continue;
 
+        }
         }
 
         update(board, direction);
-        if (!hasEmptySpaces(board)) {
+
+        if (!hasEmptySpaces(board))
+        {
+            print(board);
+            std::cout << "Can't add new tile, you lost!" << std::endl;
+            running = false;
+            continue;
+        }
+
+        if (hasWon(board, OPTIONAL_GOAL))
+        {
+            print(board);
+            std::cout << "You reached the goal value of " << OPTIONAL_GOAL << "!" << std::endl;
+            running = false;
+            continue;
+        }
+        else if (hasWon(board, DEFAULT_GOAL))
+        {
+            print(board);
+            std::cout << "You reached the goal value of 2048!" << std::endl;
+            running = false;
+            continue;
+        }
+
+        if (gameOver(board))
+        {
+            print(board);
             std::cout << "Can't add new tile, you lost!" << std::endl;
             running = false;
             continue;
@@ -387,18 +477,5 @@ int main() {
 
         newValue(&board, randomEng, distr);
         print(board);
-
-        if (hasWon(board, OPTIONAL_GOAL)) {
-            std::cout << "You reached the goal value of " << OPTIONAL_GOAL << "!" << std::endl;
-            running = false;
-        } else if (hasWon(board, DEFAULT_GOAL)) {
-            std::cout << "You reached the goal value of 2048!" << std::endl;
-            running = false;
-        }
-
-        if (gameOver(board)) {
-            std::cout << "Can't add new tile, you lost!" << std::endl;
-            running = false;
-        }
     }
 }
